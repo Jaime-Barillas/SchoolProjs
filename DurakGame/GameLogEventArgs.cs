ee8376ee8b2129f4bb2f1cb60ba7fcfa7aa5d219
Ryan Sim <ryan.sim@dcmail.ca>
2019-03-08 14:42:58 -0500

    Added GameLogEventArgs class for handling events which should display information to the game log.

---
+++ DurakGame/GameLogEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakGame
+{
+    /// <summary>
+    /// Event data for Durak console output events.
+    /// </summary>
+    public class GameLogEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Message to be displayed to the game log.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Instantiates a new set of event arguments for Durak console output events.
+        /// </summary>
+        /// <param name="message">The message to display in the game log.</param>
+        public GameLogEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
